c778c88e8bbac1c5e4215cd221bd19861b1728cd
Nithin N P <nithin.n.p@pwc.com>
2025-04-29 13:51:27 +0530

    updated

--- eks.tf
+++ eks.tf
@@ -11,15 +11,15 @@ module "eks" {
   vpc_id     = var.vpc_id
 
   eks_managed_node_groups = {
-    cmk_encrypted_nodes = {
+    unencrypted_nodes = {
       desired_size = var.desired_capacity
       max_size     = 3
       min_size     = 1
 
       instance_types = [var.node_instance_type]
       disk_size      = 20  # EBS volume, unencrypted by default
-      encrypted       = true
-      kms_key_id      = aws_kms_key.eks_cmk.arn  # or use var.kms_key_id
+      encrypted       = false
+      # kms_key_id      = aws_kms_key.eks_cmk.arn  # or use var.kms_key_id
 
       tags = {
         Name = "eks-node-unencrypted-poc"
